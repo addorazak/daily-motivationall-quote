0045088a7aa80e359756567cb93f552c7ddd099d
Razak <addorazak8@gmail.com>
2023-03-03 17:36:53 +0100

    refactored code with multiple recipient emails

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from email.mime.text import MIMEText
 import smtplib
 import datetime as dt
 from random import choice
 
 MY_EMAIL = "email"
 MY_PASSWORD = "password"
+RECIPIENTS = ["firstperson@gmail.com", "secondperson@yahoo.com", "thirdperson@gmail.com", "fourthperson@gmail.com"]
 
 now = dt.datetime.now()
 weekday = now.weekday()
@@ -12,16 +14,22 @@ hour = time.hour
 
 list_of_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-if hour == 8:
-    with open("quotes.txt") as quote_file:
-        all_quotes = quote_file.readlines()
-        quote = choice(all_quotes)
+with open("quotes.txt") as quote_file:
+    all_quotes = quote_file.readlines()
+    quote = choice(all_quotes)
 
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs="email@gmail.com",
-            msg=f"Subject:{list_of_days[weekday]} Motivation\n\n{quote}"
-        )
+if hour == 8:
+    for email in RECIPIENTS:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+
+            msg = MIMEText(f"{quote}")
+            msg["Subject"] = f"{list_of_days[weekday]} Motivation"
+            msg["From"] = MY_EMAIL
+            msg["To"] = email
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=email,
+                msg=msg.as_string()
+            )
